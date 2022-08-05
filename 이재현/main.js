6056dddef95fcd06903f2c4a45af73729f168f9b
icaosn <108523704+icaosn@users.noreply.github.com>
2022-08-05 19:45:17 +0900

    8/5 노드

---
+++ 이재현/main.js
@@ -0,0 +1,15 @@
+var a = 10
+let b = 20
+const c = 30
+
+console.log(`a = ${a}, b =%{b}, c = ${c}`)
+
+for (var i = 0; i < 10; i+=2){
+    console.log(i)
+}
+
+var i = 0
+while(i<10){
+    console.log(i)
+    i += 1
+}
